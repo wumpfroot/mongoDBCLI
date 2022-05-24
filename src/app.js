5da1251eb12c1857535130cb6aee25fcfc263c28
wumpfroot <markuskojo@msn.com>
2022-05-24 18:31:46 +0100

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const yargs = require("yargs");
 const { client, connection } = require("./db/connection");
-const { addMovie, listMovies } = require("./utils")
+const { addMovie, listMovies, deleteMovie, updateMovie } = require("./utils");
 
 const app = async (yargsObj) => {
     const collection = await connection();
@@ -8,10 +8,16 @@ const app = async (yargsObj) => {
         if (yargsObj.add) {
             //add movie to mongodb
             await addMovie({ title: yargsObj.title, actor: yargsObj.actor, year: yargsObj.year }, collection);
-        } else if (yargsObj.list) {
+        }else if (yargsObj.update) {
+            //update movie from mongodb
+            await updateMovie({ title: yargsObj.title}, {$set: {director: yargsObj.director}}, collection);
+        }else if (yargsObj.delete) {
+            //delete movie from mongodb
+            await deleteMovie({title: yargsObj.title}, collection);
+        }else if (yargsObj.list) {
             //list movies from mongodb
             await listMovies(collection);
-        } else {
+        }else {
             console.log("Incorrect command")
         }
     } catch (error) {
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 exports.addMovie = async (movieObj, collection) => {
-    const response = await collection.insertOne({ title: movieObj.title })
+    const response = await collection.insertOne({ title: movieObj.title, actor: movieObj.actor, year: movieObj.year })
     if (response.acknowledged) {
         console.log("Succesfully added a movie")
     } else {
@@ -11,4 +11,19 @@ exports.addMovie = async (movieObj, collection) => {
 exports.listMovies = async (collection) => {
     const movies = await collection.find().toArray();
     console.log(movies)
+}
+
+// exports.updateMovie = async (movieObj, collection) => {
+//     const editMovie = await collection.updateOne({ title: movieObj.title} );
+//     console.log(editMovie)
+// }
+
+exports.deleteMovie = async (movieObj, collection) => {
+    const delMovie = await collection.deleteOne({ title: movieObj.title });
+    if (delMovie.deletedCount === 1) {
+        console.log("Succesfully deleted the movie")
+    } else {
+        console.log("Something went wrong")
+    }
+    console.log(delMovie); 
 }
